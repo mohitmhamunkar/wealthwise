95c814418f5edb35d984bc6e1eb9992d3fb7ee1b
Aakash Singh <>
2023-04-22 11:50:09 -0400

    Added login , register layouts and login activity changes

--- app/src/main/java/edu/northeastern/wealthwise/LoginActivity.java
+++ app/src/main/java/edu/northeastern/wealthwise/LoginActivity.java
@@ -1,14 +1,77 @@
 package edu.northeastern.wealthwise;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
+    EditText username;
+    EditText password;
+
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        username = findViewById(R.id.userName);
+        password = findViewById(R.id.password);
+
+        Button loginButton = findViewById(R.id.loginBtn);
+
+        loginButton.setOnClickListener(v -> doLogin(username, password));
+    }
+
+    private void doLogin(EditText username, EditText password) {
+        String userStr = String.valueOf(username.getText());
+        String passwordStr = String.valueOf(password.getText());
+
+        if (TextUtils.isEmpty(userStr)) {
+            Toast.makeText(LoginActivity.this, "Please enter Username!", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (TextUtils.isEmpty(passwordStr)) {
+            Toast.makeText(LoginActivity.this, "Please enter Password!", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(userStr, passwordStr)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Toast.makeText(LoginActivity.this, "Login Successful!",
+                                Toast.LENGTH_SHORT).show();
+                        Log.d(TAG, "signInWithEmail:success");
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(intent);
+                        finish();
+
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "signInWithEmail:failure", task.getException());
+                        Toast.makeText(LoginActivity.this, "Login failed!",
+                                Toast.LENGTH_SHORT).show();
+
+                    }
+                });
     }
 }
