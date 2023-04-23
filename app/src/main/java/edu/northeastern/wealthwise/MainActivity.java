91aa1fbd7c10f10a555ba8c978999a23225fc062
mohitmhamunkar <mhamunkar.m@northeastern.edu>
2023-04-23 05:18:03 -0400

    Stats UI updated
    Pie Chart libraries added which dummy data, activities created and updated

--- app/src/main/java/edu/northeastern/wealthwise/MainActivity.java
+++ app/src/main/java/edu/northeastern/wealthwise/MainActivity.java
@@ -3,21 +3,32 @@ package edu.northeastern.wealthwise;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ImageView home;
+    private TextView homeText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        home = findViewById(R.id.transactionView);
+        homeText = findViewById(R.id.homeText);
+        home.setImageResource(R.mipmap.transaction_selected_foreground);
+        homeText.setTextColor(Color.parseColor("#FF8D45"));
     }
 
     public void onClick(View view){
         int clickId = view.getId();
         if(clickId == R.id.addTransactionBtn) {
             startActivity(new Intent(this, TransactionActivity.class));
+        }
+        if(clickId == R.id.statsView) {
+            startActivity(new Intent(this, StatsActivity.class));
         }
     }
 }
--- app/src/main/java/edu/northeastern/wealthwise/StatsActivity.java
+++ app/src/main/java/edu/northeastern/wealthwise/StatsActivity.java
@@ -2,13 +2,48 @@ package edu.northeastern.wealthwise;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.eazegraph.lib.charts.PieChart;
+import org.eazegraph.lib.models.PieModel;
 
 public class StatsActivity extends AppCompatActivity {
-
+    private PieChart pieChart;
+    private ImageView statsView;
+    private TextView statsText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stats);
+        statsView = findViewById(R.id.statsView);
+        statsText = findViewById(R.id.statsText);
+        statsText.setTextColor(Color.parseColor("#FF8D45"));
+        statsView.setImageResource(R.mipmap.graph_selected_foreground);
+        pieChart = findViewById(R.id.piechart);
+        pieChart.addPieSlice(
+                new PieModel(
+                        "R",
+                        40,
+                        Color.parseColor("#FFA726")));
+        pieChart.addPieSlice(
+                new PieModel(
+                        "Python",
+                        30,
+                        Color.parseColor("#66BB6A")));
+        pieChart.addPieSlice(
+                new PieModel(
+                        "C++",
+                        5,
+                        Color.parseColor("#EF5350")));
+        pieChart.addPieSlice(
+                new PieModel(
+                        "Java",
+                        25,
+                        Color.parseColor("#29B6F6")));
+        pieChart.startAnimation();
+        pieChart.setLabelFor(0);
     }
 }
