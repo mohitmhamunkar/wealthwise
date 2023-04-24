9d32fcd75736a0ae215b5d0696995b2fea624437
Aakash Singh <>
2023-04-23 20:25:31 -0400

    Added goalsItem class and recycler view holder

---
+++ app/src/main/java/edu/northeastern/wealthwise/GoalsRecyclerViewHolder.java
@@ -0,0 +1,20 @@
+package edu.northeastern.wealthwise;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class GoalsRecyclerViewHolder extends RecyclerView.ViewHolder{
+    public TextView category;
+    public ImageView colorCircle;
+    public TextView amount;
+
+    public GoalsRecyclerViewHolder(@NonNull View itemView) {
+        super(itemView);
+        category = itemView.findViewById(R.id.categoryGoal);
+        colorCircle = itemView.findViewById(R.id.circleView);
+    }
+}
--- app/src/main/java/edu/northeastern/wealthwise/datamodels/GoalsItem.java
+++ app/src/main/java/edu/northeastern/wealthwise/datamodels/GoalsItem.java
@@ -1,5 +1,27 @@
 package edu.northeastern.wealthwise.datamodels;
 
 public class GoalsItem {
+    public String amount;
+    public String category;
 
+    public GoalsItem(String amount, String category) {
+        this.amount = amount;
+        this.category = category;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
 }
